89c4c18441a9ca011131ca13a110c7a53eb7ccdc
Salvador Peynado <info@prointernet.mx>
2015-09-11 10:42:52 -0500

    inicio de proyecto

---
+++ js/juzgados/juzgados.js
@@ -0,0 +1,88 @@
+var Zacatecas=new Array()
+	Zacatecas[0] = new slctr('Juzgado:')
+	Zacatecas[1] = new slctr("1o Civil",'1o Civil')
+	Zacatecas[2] = new slctr("2o Civil",'2o Civil')		
+	Zacatecas[3] = new slctr("3o Civil",'3o Civil')	
+	Zacatecas[4] = new slctr("1o Familiar",'1o Familiar')	
+	Zacatecas[5] = new slctr("2o Familiar",'2o Familiar')	
+	Zacatecas[6] = new slctr("3o Familiar",'3o Familiar')	
+	Zacatecas[7] = new slctr("1o Mercantil",'1o Mercantil')	
+	Zacatecas[8] = new slctr("2o Mercantil",'2o Mercantil')	
+	Zacatecas[9] = new slctr("3o Mercantil",'3o Mercantil')	
+	Zacatecas[10] = new slctr("1o Penal",'1o Penal')	
+	Zacatecas[11] = new slctr("2o Penal",'2o Penal')	
+
+var Fresnillo=new Array()
+	Fresnillo[0] = new slctr('Juzgado:')
+	Fresnillo[1] = new slctr("Civil",'Civil')
+	Fresnillo[2] = new slctr("1o Familiar",'1o Familiar')	
+	Fresnillo[3] = new slctr("2o Familiar",'2o Familiar')
+	Fresnillo[4] = new slctr("Mercantil",'Mercantil')	
+	Fresnillo[5] = new slctr("1o Penal",'1o Penal')	
+	Fresnillo[6] = new slctr("2o Penal",'2o Penal')	
+
+var Jerez=new Array()
+	Jerez[0] = new slctr('Juzgado:')
+	Jerez[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var RioGrande=new Array()
+	RioGrande[0] = new slctr('Juzgado:')
+	RioGrande[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Sombrerete=new Array()
+	Sombrerete[0] = new slctr('Juzgado:')
+	Sombrerete[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+	
+var Tlaltenango=new Array()
+	Tlaltenango[0] = new slctr('Juzgado:')
+	Tlaltenango[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Calera=new Array()
+	Calera[0] = new slctr('Juzgado:')
+	Calera[1] = new slctr("Civil",'Civil')
+	Calera[2] = new slctr("Penal",'Penal')	
+	Calera[3] = new slctr("Mixto",'Mixto')
+
+var ConcepcióndelOro=new Array()
+	ConcepcióndelOro[0] = new slctr('Juzgado:')
+	ConcepcióndelOro[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Jalpa=new Array()
+	Jalpa[0] = new slctr('Juzgado:')
+	Jalpa[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Juchipila=new Array()
+	Juchipila[0] = new slctr('Juzgado:')
+	Juchipila[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Loreto=new Array()
+	Loreto[0] = new slctr('Juzgado:')
+	Loreto[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var MiguelAuza=new Array()
+	MiguelAuza[0] = new slctr('Juzgado:')
+	MiguelAuza[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Nochistlan=new Array()
+	Nochistlan[0] = new slctr('Juzgado:')
+	Nochistlan[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var OjoCaliente=new Array()
+	OjoCaliente[0] = new slctr('Juzgado:')
+	OjoCaliente[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Pinos=new Array()
+	Pinos[0] = new slctr('Juzgado:')
+	Pinos[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var TeuldeGonzalezOrtega=new Array()
+	TeuldeGonzalezOrtega[0] = new slctr('Juzgado:')
+	TeuldeGonzalezOrtega[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Valparaiso=new Array()
+	Valparaiso[0] = new slctr('Juzgado:')
+	Valparaiso[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
+
+var Villanueva=new Array()
+	Villanueva[0] = new slctr('Juzgado:')
+	Villanueva[1] = new slctr("1a Instancia y Familiar",'1a Instancia y Familiar')
---
+++ js/tipos/tipos_inserciones.js
@@ -0,0 +1,49 @@
+var Particular=new Array()
+	Particular[0] = new slctr('Categoria:')
+	Particular[1] = new slctr("Informes Comerciales",'Informes Comerciales')
+	Particular[2] = new slctr("Otro",'Otro')		
+
+var Gobierno=new Array()
+	Gobierno[0] = new slctr('Categoria:')
+	Gobierno[1] = new slctr("Nombramientos",'Nombramientos')
+	Gobierno[2] = new slctr("Informe Varios",'Informe Varios')		
+	Gobierno[3] = new slctr("Informe Otros",'Informe Otros')		
+
+var Legislativo=new Array()
+	Legislativo[0] = new slctr('Categoria:')
+	Legislativo[1] = new slctr("Leyes",'Leyes')
+	Legislativo[2] = new slctr("Reformas",'Reformas')		
+	Legislativo[3] = new slctr("Decretos",'Decretos')		
+	Legislativo[4] = new slctr("Código",'Código')		
+
+var Judicial=new Array()
+	Judicial[0] = new slctr('Categoria:')
+	Judicial[1] = new slctr("Resoluciones",'Resoluciones')
+	Judicial[2] = new slctr("Notificaciones",'Notificaciones')		
+	Judicial[3] = new slctr("Avisos",'Avisos')		
+	Judicial[4] = new slctr("Sentencias",'Sentencias')		
+
+var Fraccionamientos=new Array()
+	Fraccionamientos[0] = new slctr('Categoria:')
+	Fraccionamientos[1] = new slctr("Avisos de Afectación",'Avisos de Afectación')
+	Fraccionamientos[2] = new slctr("Permisos de Construcción",'Permisos de Construcción')		
+	Fraccionamientos[3] = new slctr("Notificaciones",'Notificaciones')		
+	Fraccionamientos[4] = new slctr("Revocaciones",'Revocaciones')		
+
+var Edictos=new Array()
+	Edictos[0] = new slctr('Categoria:')
+	Edictos[1] = new slctr("Civil",'Civil')
+	Edictos[2] = new slctr("Mercantil",'Mercantil')		
+	Edictos[3] = new slctr("Otro",'Otro')		
+
+var Otros=new Array()
+	Otros[0] = new slctr('Categoria:')
+	Otros[1] = new slctr("Acuerdos",'Acuerdos')
+	Otros[2] = new slctr("Reglamentos",'Reglamentos')		
+	Otros[3] = new slctr("Convocatorias",'Convocatorias')		
+	Otros[4] = new slctr("Nombramientos",'Nombramientos')		
+	Otros[5] = new slctr("Expropiaciones",'Expropiaciones')		
+	Otros[6] = new slctr("Aviso Notarial",'Aviso Notarial')		
+	
+	
+
